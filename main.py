194b3b556233cc26e7878fd2b32210c9a796a685
LancelotWinterLord <131851776+LancelotWinterLord@users.noreply.github.com>
2023-09-16 11:30:50 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+# 1
+number = 9
+float_number = 9.0
+
+msg = 19
+a = 1
+b = 2
+print("a =" + str(a))
+print("b =" + str(b))
+
+# 2
+var_int = 42
+var_flt = 42.0
+var_int1 = 42.0
+
+print(var_int1)
+
+# 3
+name = input("Enter your name")
+print("The name is" + name)
+
+age = input("Enter your age")
+print("The name is" + age)
+
+# 4
+name1 = 'C3PO'
+age1 = 112
+print(f"Salem, my name is {name1} and i am age {age1} years old")
+
+color = 'gold'
+print(f"This is have a color like as {color}")
+
+# 5
+try:
+    num = int(input("Enter the num: "))
+    result = 10/num
+    print("Result", result)
+except:
+    print("Division by zero is not allowed")
+
+# practice
+Name_Company = 'LLC Company'
+Num_Employee = 65
+Locate = 'Ireland'
+Low_Price = 15
+High_Price = 300
+
+print(f"We are {Name_Company} located in {Locate}. Our {Num_Employee} employees can help you find the policy."
+      f"Our price low {Low_Price} and high {High_Price}")
